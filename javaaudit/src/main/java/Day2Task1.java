b730cd465cc4d09123eda9b8033377cd886c9e0b
heiyu <304429477@qq.com>
2022-09-08 16:35:52 +0800

    初始化

---
+++ javaaudit/src/main/java/Day2Task1.java
@@ -0,0 +1,43 @@
+/**
+ * 使用switch语句改写下列if语句：
+ * int a = 3;
+ *   int x = 100;
+ *   if(a==1)
+ *         x+=5;
+ *     else if(a==2)
+ *         x+=10;
+ *     else if(a==3)
+ *         x+=16;
+ *     else if(a==4)
+ *         x+=34;
+ *      else
+ *         x += 100;
+ */
+
+public class Day2Task1 {
+    public static void main(String[] args) {
+            int x =100;
+            int a= 3;
+            switch (a){
+                case 1:
+                    x+=5;
+                    System.out.println(x);
+                    break;
+                case 2:
+                    x+=10;
+                    System.out.println(x);
+                    break;
+                case 3:
+                    x+=16;
+                    System.out.println(x);
+                    break;
+                case 4:
+                    x+=34;
+                    System.out.println(x);
+                default:
+                    x+=100;
+                    System.out.println(x);
+                    break;
+            }
+    }
+}
